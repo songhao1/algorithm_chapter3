d747dafbbfc481a4d0d04d34aa376d1e7e12a451
songhao1 <717977831@qq.com>
2020-07-09 00:42:49 +0800

    2020/7/9

--- 源.cpp
+++ 源.cpp
@@ -71,3 +71,64 @@
 //	}
 //	return 0;
 //}
+
+
+//#include<cstdio>
+//
+////PAT B1022
+//
+//int main() {
+//	int A, B, D;
+//	scanf("%d %d %d", &A, &B, &D);
+//
+//	int sum = A + B;
+//
+//	int z[31], num = 0;//z存放D进制的每一位
+//	do {
+//		z[num++] = sum % D;
+//		sum = sum / D;
+//	} while (sum != 0);
+//
+//	for (int i = num - 1; i >= 0; i--) {
+//		printf("%d", z[i]);
+//	}
+//
+//	return 0;
+//}
+
+
+//#include<cstdio>
+//#include<cstring>
+//#include<iostream>
+//
+//using namespace std;
+//
+////PAT B1009
+//
+//int main() {
+//	char str[90];
+//	cin.getline(str, 90);//<<算法笔记>>中的gets(str)报错了
+//
+//	int len = strlen(str);
+//	int r = 0, h = 0;//r为行,h为列
+//	char ans[90][90];//ans[0]-ans[r]存放单词
+//
+//	for (int i = 0; i < len; i++) {
+//		if (str[i] != ' ') {	//如果不是空格,则存放至ans[r][h],并令h++
+//			ans[r][h++] = str[i];
+//		}
+//		else {	//如果是空格,说明一个单词结束,行r增加1,列h恢复至0
+//			ans[r][h] = '\0';	//	末尾是结束符\0
+//			r++;
+//			h = 0;
+//		}
+//	}
+//	ans[r][h] = '\0';//<<算法笔记>>中没有这一句,应该加上
+//
+//	for (int i = r; i >= 0; i--) {
+//		printf("%s", ans[i]);
+//		if (i > 0) printf(" ");
+//	}
+//
+//	return 0;
+//}
